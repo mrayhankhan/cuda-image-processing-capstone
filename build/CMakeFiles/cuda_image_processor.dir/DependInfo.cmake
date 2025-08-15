f8bfa165c480c9d5d8dfa330d4ba1d7a3438d036
M Rayhan Khan <rayhan22269@iiitd.ac.in>
2025-08-15 11:05:23 +0000

    final

---
+++ build/CMakeFiles/cuda_image_processor.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/huihui/src/image_processor.cu" "CMakeFiles/cuda_image_processor.dir/src/image_processor.cu.o" "gcc" "CMakeFiles/cuda_image_processor.dir/src/image_processor.cu.o.d"
+  "/workspaces/huihui/src/cpu_reference.cpp" "CMakeFiles/cuda_image_processor.dir/src/cpu_reference.cpp.o" "gcc" "CMakeFiles/cuda_image_processor.dir/src/cpu_reference.cpp.o.d"
+  "/workspaces/huihui/src/main.cpp" "CMakeFiles/cuda_image_processor.dir/src/main.cpp.o" "gcc" "CMakeFiles/cuda_image_processor.dir/src/main.cpp.o.d"
+  "/workspaces/huihui/src/utils.cpp" "CMakeFiles/cuda_image_processor.dir/src/utils.cpp.o" "gcc" "CMakeFiles/cuda_image_processor.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cuda_image_processor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_image_processor.dir/src/cpu_reference.cpp.o"
+  "CMakeFiles/cuda_image_processor.dir/src/cpu_reference.cpp.o.d"
+  "CMakeFiles/cuda_image_processor.dir/src/image_processor.cu.o"
+  "CMakeFiles/cuda_image_processor.dir/src/image_processor.cu.o.d"
+  "CMakeFiles/cuda_image_processor.dir/src/main.cpp.o"
+  "CMakeFiles/cuda_image_processor.dir/src/main.cpp.o.d"
+  "CMakeFiles/cuda_image_processor.dir/src/utils.cpp.o"
+  "CMakeFiles/cuda_image_processor.dir/src/utils.cpp.o.d"
+  "bin/cuda_image_processor"
+  "bin/cuda_image_processor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/cuda_image_processor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
